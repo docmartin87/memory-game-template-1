16c6823eeafa298cdc2f2f02ba34ecd8705b2acc
docmartin87 <doc.martin87@gmail.com>
2021-01-10 01:27:06 +0000

    card game running

--- main.js
+++ main.js
@@ -1,74 +1,74 @@
-  document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
 
     //Load Card Array
-    const cardsArray = [
-        {name: "Ace of Hearts", img: "images/ace-hearts.png"},
-        {name: "Ace of Hearts", img: "images/ace-hearts.png"},
-        {name: "Ace of Spades", img: "images/ace-spades.png"},
-        {name: "Ace of Spades", img: "images/ace-spades.png"},
-        {name: "King of Hearts", img: "images/king-hearts.png"},
-        {name: "King of Hearts", img: "images/king-hearts.png"},
-        {name: "King of Spades", img: "images/king-spades.png"},
-        {name: "King of Spades", img: "images/king-spades.png"},
-        {name: "Queen of Hearts", img: "images/queen-hearts.png"},
-        {name: "Queen of Hearts", img: "images/queen-hearts.png"},
-        {name: "Queen of Spades", img: "images/queen-spades.png"},
-        {name: "Queen of Spades", img: "images/queen-spades.png"},
-        {name: "Jack of Hearts", img: "images/jack-hearts.png"},
-        {name: "Jack of Hearts", img: "images/jack-hearts.png"},
-        {name: "Jack of Spades", img: "images/jack-spades.png"},
-        {name: "Jack of Spades", img: "images/jack-spades.png"},
+    const cardArray = [
+        {name: 'Ace of Hearts', img: 'images/ace-hearts.jpg'},
+        {name: 'Ace of Hearts', img: 'images/ace-hearts.jpg'},
+        {name: 'Ace of Spades', img: 'images/ace-spades.jpg'},
+        {name: 'Ace of Spades', img: 'images/ace-spades.jpg'},
+        {name: 'King of Hearts', img: 'images/king-hearts.jpg'},
+        {name: 'King of Hearts', img: 'images/king-hearts.jpg'},
+        {name: 'King of Spades', img: 'images/king-spades.jpg'},
+        {name: 'King of Spades', img: 'images/king-spades.jpg'},
+        {name: 'Queen of Hearts', img: 'images/queen-hearts.jpg'},
+        {name: 'Queen of Hearts', img: 'images/queen-hearts.jpg'},
+        {name: 'Queen of Spades', img: 'images/queen-spades.jpg'},
+        {name: 'Queen of Spades', img: 'images/queen-spades.jpg'},
+        {name: 'Jack of Hearts', img: 'images/jack-hearts.jpg'},
+        {name: 'Jack of Hearts', img: 'images/jack-hearts.jpg'},
+        {name: 'Jack of Spades', img: 'images/jack-spades.jpg'},
+        {name: 'Jack of Spades', img: 'images/jack-spades.jpg'},
     ];
 
-    cardsArray.sort(() => 0.5 - Math.random());
+    cardArray.sort(() => 0.5 - Math.random());
 
-    const table = document.querySelector(".table");
-    const scoreDisplay = document.querySelector("#score");
+    const grid = document.querySelector('.grid');
+    const resultDisplay = document.querySelector('#result');
     var cardsChosen = [];
     var cardsChosenId = [];
     var cardsWon = [];
 
     // Board Creation
     function createBoard(){
-        for (let i =0; i < cardsArray.length; i++){
-            var card = document.createElement("img");
-            card.setAttribute("src", "images/question-marks.jpg");
-            card.setAttribute("data-id", i);
-            card.addEventListener("click", turncard);
-            table.appendChild(card);
+        for (let i =0; i < cardArray.length; i++){
+            var card = document.createElement('img');
+            card.setAttribute('src', 'images/cover.jpg');
+            card.setAttribute('data-id', i);
+            card.addEventListener('click', turncard);
+            grid.appendChild(card);
         }
     }
 
 
   //matches?
   function checkForMatch(){
-      var cards = document.querySelectorAll("img");
+      var card = document.querySelectorAll('img');
       const optionOneId = cardsChosenId[0];
       const optionTwoId = cardsChosenId[1];
       if (cardsChosen[0] === cardsChosen[1]){
-          alert("MATCH");
-          cards[optionOneId].setAttribute("src", "images/grey.png");
-          cards[optionTwoId].setAttribute("src", "images/grey.png");
+          alert('MATCH');
+          card[optionOneId].setAttribute('src', 'images/grey.jpg');
+          card[optionTwoId].setAttribute('src', 'images/grey.jpg');
           cardsWon.push(cardsChosen);
       }else{
-          cards[optionOneId].setAttribute("src", "images/question-marks.jpg");
-          cards[optionTwoId].setAttribute("src", "images/question-marks.jpg");
-          alert("Try Again...");
+          card[optionOneId].setAttribute('src', 'images/cover.jpg');
+          card[optionTwoId].setAttribute('src', 'images/cover.jpg');
+          alert('Try Again...');
         };
         cardsChosen = [];
         cardsChosenId = [];
-        scoreDisplay.textContent === cardsWon.length
-        if (cardsWon.length === cardsArray.length/2) {
-            scoreDisplay.textContent = "You Beat Me"
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = 'You Beat Me';
         };
   };
 
   //flip card
   function turncard(){
-      var cardId = this.getAttribute("data-id");
-      cardsChosen.push(cardsArray[cardId.name]);
+      var cardId = this.getAttribute('data-id');
+      cardsChosen.push(cardArray[cardId].name);
       cardsChosenId.push(cardId);
-      this.setAttribute("src", cardsArray[cardId].img);
+      this.setAttribute('src', cardArray[cardId].img);
       if (cardsChosen.length === 2){
           setTimeout(checkForMatch, 500);
       };
